4e4277811fc51e5733fa59d4050b059b6c84f112
fahmirevo <fahmi.velosa1997@gmail.com>
2018-06-07 19:04:54 +0700

    remove multiprocess and verbosity

--- cnn.py
+++ cnn.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     model.fit_generator(generator=data_generator(batch_size=batch_size),
                         steps_per_epoch=steps_per_epoch,
                         epochs=epochs,
-                        use_multiprocessing=True)
+                        verbose=0)
     model.save("cnn.model")
 
     # model.fit(x_train, y_train,
