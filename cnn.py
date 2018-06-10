aa62b8038a0058822208175e8ad61276b1456cea
fahmirevo <fahmi.velosa1997@gmail.com>
2018-06-10 12:11:35 +0700

    re structure

--- cnn.py
+++ cnn.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 # from keras import backend as K
 
@@ -18,17 +18,6 @@ img_rows, img_cols = 64, 64
 
 
 if __name__ == '__main__':
-    # data = Data()
-    # x_train, y_train = data.train
-    # x_test, y_test = data.test
-
-    # if K.image_data_format() == 'channels_first':
-    #     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    #     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    #     input_shape = (1, img_rows, img_cols)
-    # else:
-    #     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    #     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
     input_shape = (img_rows, img_cols, 1)
 
@@ -36,14 +25,27 @@ if __name__ == '__main__':
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
                      input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
+
     model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
     model.add(Dropout(0.5))
+
     model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
     model.add(Dropout(0.5))
+
     model.add(Dense(num_classes, activation='softmax'))
 
     model.compile(loss=keras.losses.categorical_crossentropy,
@@ -56,13 +58,3 @@ if __name__ == '__main__':
                         steps_per_epoch=steps_per_epoch,
                         epochs=epochs)
     model.save("model")
-
-    # model.fit(x_train, y_train,
-    #           batch_size=batch_size,
-    #           epochs=epochs,
-    #           verbose=1,
-    #           validation_data=(x_test, y_test))
-    # score = model.evaluate(x_test, y_test, verbose=0)
-
-    # print('Test loss:', score[0])
-    # print('Test accuracy:', score[1])
